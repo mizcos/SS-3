1254b324f47a71f62d573df3a8ebba285ac58424
koh <iamtossup@gmail.com>
2020-12-26 16:58:22 +0900

    add top page ui \
    トップページのUIを試作しました．デザイン等はまだですが，内部関数の参考に

---
+++ disp.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5 import QtWidgets
+from top_ui import Ui_TopWindow
+
+
+class gui(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(gui, self).__init__(parent)
+        self.ui = Ui_TopWindow()
+        self.ui.setupUi(self)
+
+    def pressed_setting (self):
+        a=1#設定ボタンが押されたときの処理
+
+    def pressed_mdcn_info(self):
+        a=1#お薬情報ボタンが押されたときの処理
+    
+    def pressed_mdcn_record(self):
+        a=1#服薬記録ボタンが押されたときの処理
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = gui()
+    window.show()
+    sys.exit(app.exec_())
