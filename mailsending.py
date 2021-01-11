a56a1799852b08f51c294c903d92c2acf26ff260
wangkuo2008 <45811103+wangkuo2008@users.noreply.github.com>
2021-01-11 15:28:07 +0900

    メール発送機能
    メール発送機能のcoding

---
+++ mailsending.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+from email.header import Header
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from datetime import datetime
+dt=datetime.now()
+
+def send_mail():
+    try:
+        #mail server
+        host_server = 'smtp.gmail.com'
+        #mail user name
+        sender_qq = 'CPI20.group1@gmail.com'
+        # password
+        pwd = 'CPI2020g1'
+        # sender email
+        sender_qq_mail = 'CPI20.group1@gmail.com'
+        # receiver email
+        receiver = 'CPI20.group1@gmail.com'
+
+        # content
+        mail_content = 'Titech様\n \n今日の服薬記録を送ります。\n \n名前:システム制御さん\n時間:' + dt.strftime( '%H:%M:%S' )        +'\n薬:龍角散\n薬を服用しない理由:なし\n備考:なし\n \n----\n服薬記録サービス'
+        # title
+        mail_title = '服薬記録送信'+dt.strftime( '%Y-%m-%d %H:%M:%S' )
+
+        # ssl
+        smtp = SMTP_SSL(host_server)
+        # set_debuglevel()
+        smtp.set_debuglevel(1)
+        smtp.ehlo(host_server)
+        smtp.login(sender_qq, pwd)
+
+        msg = MIMEText(mail_content, "plain", 'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
+        msg["From"] = sender_qq_mail
+        msg["To"] = receiver
+        smtp.sendmail(sender_qq_mail, receiver, msg.as_string())
+        smtp.quit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+if __name__ == '__main__':
+    if send_mail():
+        0
+
